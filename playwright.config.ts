76dacbc7ee99473563b84b6a83cb18c4b7b961f5
Olga <olga.letinskaya@tateeda.com>
2026-03-29 19:40:06 +0300

    Initialize Playwright project

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  timeout: 30000,
+
+  use: {
+    baseURL: 'https://qauto.forstudy.space',
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    },
+  ],
+});
