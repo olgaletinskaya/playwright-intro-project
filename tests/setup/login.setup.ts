e180330d0f671b6793ac3a9d4d90379fe728c079
Olga <olga.letinskaya@tateeda.com>
2026-04-21 22:26:35 +0300

    refactor: use env variables in login setup

--- tests/setup/login.setup.ts
+++ tests/setup/login.setup.ts
@@ -1,21 +1,22 @@
 import { test } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
-
-
 test('login and save storage state', async ({ page }) => {
-  console.log('EMAIL:');
-  console.log('PASSWORD:');
+  // Теперь код берет данные из ворклоу или .env файла
+  const email = process.env.EMAIL || 'abbiesun806test+1@gmail.com'; 
+  const password = process.env.PASSWORD || 'Password123!';
 
   await page.goto('/');
 
   await page.getByRole('button', { name: 'Sign In' }).click();
 
-  await page.locator('#signinEmail').fill('abbiesun806test+1@gmail.com');
-  await page.locator('#signinPassword').fill('Password123!');
+  // Используем переменные вместо текста
+  await page.locator('#signinEmail').fill(email);
+  await page.locator('#signinPassword').fill(password);
 
   await page.getByRole('button', { name: 'Login' }).click();
 
+  // Ждем перехода, это подтвердит успешный вход
   await page.waitForURL(/garage/);
 
   await page.context().storageState({ path: 'storageState.json' });
